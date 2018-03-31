317aa0eee0db2620497f4c677b4bf672ffd1a000
soohyeonie <2013033154@hanyang.ac.kr>
2018-04-01 05:16:14 +0900

    14891
    Second commit

--- baekjoon/14891.cpp
+++ baekjoon/14891.cpp
@@ -5,10 +5,19 @@ using namespace std;
 int wheel[4][8];
 bool isRotated[4];
 
+void go(int i,int n) {
+	isRotated[i]=true;
+	
+	if(i-1>=0&&wheel[i-1][2]!=wheel[i][6]&&!isRotated[i-1]) go(i-1,-n);
+	if(i+1<4&&wheel[i][2]!=wheel[i+1][6]&&!isRotated[i+1]) go(i+1,-n);
+	
+	rotate(wheel[i],wheel[i]+((n==1)?7:1),wheel[i]+8);
+}
+
 int main() {
    for(int i=0;i<4;i++) {
       for(int j=0;j<8;j++) {
-         scanf("%1d",&wheel[i][j])
+         scanf("%1d",&wheel[i][j]);
       }
    }
    
